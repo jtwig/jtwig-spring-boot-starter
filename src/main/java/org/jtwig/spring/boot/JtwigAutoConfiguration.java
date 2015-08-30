a07a8747a11c2592a6a1214167ba2ea82546952d
Joao De Melo <jmelo@lyncode.com>
2015-08-30 11:50:47 +0100

    Changed setter name

--- src/main/java/org/jtwig/spring/boot/JtwigAutoConfiguration.java
+++ src/main/java/org/jtwig/spring/boot/JtwigAutoConfiguration.java
@@ -26,7 +26,7 @@ public class JtwigAutoConfiguration {
         public JtwigViewResolver jtwigViewResolver() {
             JtwigViewResolver viewResolver = new JtwigViewResolver();
             if (configuration != null) {
-                properties.setConfigurationBuilder(configuration);
+                properties.setConfiguration(configuration);
             }
             properties.applyToViewResolver(viewResolver);
             return viewResolver;
